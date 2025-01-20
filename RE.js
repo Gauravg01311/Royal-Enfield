c126b416fb4bf419916e9f7074628b352726bc64
gauravg01311 <gaurav.gavale282@gmail.com>
2025-01-20 10:04:02 +0530

    first release

---
+++ RE.js
@@ -0,0 +1,39 @@
+//for Motorcycles slide
+function car1(){
+    var interceptor=document.getElementById("change1");
+    interceptor.src="./images/carosel1.avif";
+}
+function car2(){
+    var continental=document.getElementById("change1");
+    continental.src="./images/carosel2.avif";
+}
+function car3(){
+    var supermeteor=document.getElementById("change1");
+    supermeteor.src="./images/super-meteor-650.avif";
+}
+function car4(){
+    var hunter350=document.getElementById("change1");
+    hunter350.src="./images/hunter-350-motorcycle-home.avif";
+}
+function car5(){
+    var scram411=document.getElementById("change1");
+    scram411.src="./images/scram-411.avif";
+}
+function car6(){
+    var cl350=document.getElementById("change1");
+    cl350.src="./images/classic-350-motorcycle-section.jfif";
+}
+function car7(){
+    var meteor350=document.getElementById("change1");
+    meteor350.src="./images/meteor-350.jpg" ;
+}
+function car8(){
+    var himalayanB=document.getElementById("change1");
+    himalayanB.src="./images/himalayan-black.jpg";
+}
+function maincar(){
+    var bulletjaan=document.getElementById("change1");
+    bulletjaan.src="./images/bullet-meri jaan.jpeg";
+}
+///////////////////
+
